e8377d374f6f6f4c915c2ef64cb018a317da91d1
AminSafari207 <safarin207368@gmail.com>
2025-07-18 14:43:04 +0330

    Implement Book class

---
+++ src/src/java/model/Book.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+    private int stock;
+
+    public Book(String title, String author, double price, int stock) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.stock = stock;
+    }
+}
