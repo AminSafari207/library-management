c0462ea37459fab64b0ce62d8f87bde665f8e3c2
AminSafari207 <safarin207368@gmail.com>
2025-07-18 16:00:40 +0330

    Implement updateBook & deleteBook in BookService / Refactor folder structuring

--- src/java/Main.java
+++ src/java/Main.java
@@ -8,7 +8,13 @@ public class Main {
         BookService service = new BookService();
 
         Book book1 = new Book("test 1", "test 11", 150000.500, 5);
+        Book book2 = new Book("test 2", "test 22", 300000.500, 5);
 
         service.addNewBook(book1);
+        service.addNewBook(book2);
+
+//        service.updateBook(4, 70000.750);
+
+//        service.deleteBook(4);
     }
 }
--- src/java/services/BookService.java
+++ src/java/services/BookService.java
@@ -6,13 +6,26 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BookService {
-
     public void addNewBook(Book book) throws SQLException {
         String sqlCommand = "INSERT INTO book(title, author, price, stock) VALUES ('" +
                 book.getTitle() + "','" +
                 book.getAuthor() + "'," +
                 book.getPrice() + "," +
                 book.getStock() + ")";
+
+        Statement statement = DBConnection.getConnection().createStatement();
+        statement.executeUpdate(sqlCommand);
+    }
+
+    public void updateBook(int id, double price) throws SQLException {
+        String sqlCommand = "UPDATE book SET price = " + price + " WHERE id = " + id;
+
+        Statement statement = DBConnection.getConnection().createStatement();
+        statement.executeUpdate(sqlCommand);
+    }
+
+    public void deleteBook(int id) throws SQLException {
+        String sqlCommand = "DELETE FROM book WHERE id = " + id;
 
         Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sqlCommand);
