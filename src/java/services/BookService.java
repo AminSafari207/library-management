89470a809475246b268fec8dfb9240eef2dbc0df
AminSafari207 <safarin207368@gmail.com>
2025-07-18 17:01:33 +0330

    Improve sql statement usage

--- src/java/services/BookService.java
+++ src/java/services/BookService.java
@@ -6,6 +6,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BookService {
+    private final Statement statement;
+
+    public BookService() throws SQLException {
+        this.statement = DBConnection.getConnection().createStatement();
+    }
+
     public void addNewBook(Book book) throws SQLException {
         String sqlCommand = "INSERT INTO book(title, author, price, stock) VALUES ('" +
                 book.getTitle() + "','" +
@@ -13,21 +19,18 @@ public class BookService {
                 book.getPrice() + "," +
                 book.getStock() + ")";
 
-        Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sqlCommand);
     }
 
     public void updateBook(int id, double price) throws SQLException {
         String sqlCommand = "UPDATE book SET price = " + price + " WHERE id = " + id;
 
-        Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sqlCommand);
     }
 
     public void deleteBook(int id) throws SQLException {
         String sqlCommand = "DELETE FROM book WHERE id = " + id;
 
-        Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sqlCommand);
     }
 }
