39ca45613094db81eaaffa05fe9625dcb8b40a7f
AminSafari207 <safarin207368@gmail.com>
2025-07-18 15:32:34 +0330

    Improve DBConnection class

--- src/src/java/services/DBConnection.java
+++ src/src/java/services/DBConnection.java
@@ -4,19 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
-    private static Connection connection;
+public class DBConnection  {
+    private static final String url = "jdbc:postgresql://localhost:5432/library_management";
+    private static final String user = "postgres";
+    private static final String pass = "";
 
-    public DBConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/library_management";
-        String user = "postgres";
-        String pass = "";
-
-        connection = DriverManager.getConnection(url, user, pass);
-        System.out.println("DB Connection was successful.");
-    }
-
-    public Connection getConnection() {
-        return connection;
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, pass);
     }
 }
